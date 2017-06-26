e9bed5c1b81abb294093f650c8a2df5c7461fd3f
wenhuabin <huabinwen@gmail.com>
2017-06-26 17:24:16 +0800

    添加About页面

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,6 +8,16 @@
       <li><a href="https://www.instagram.com/?hl=en" target="_blank">Instagram</a></li>
       <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
     </ul>
+    <h2>Hobby</h2>
+    <ul>
+      <li>看书</li>
+      <li>旅行</li>
+      <li>煮饭做菜</li>
+      <li>足球</li>
+      <li>羽毛球</li>
+      <li>写代码</li>
+    </ul>
+    <div>{{ $route.params.id}}</div> 
   </div>
 </template>
 
