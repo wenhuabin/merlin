c1e6f7d20f3bf0d3cd5ac0ffa4c4878c93c7b699
wenhuabin <huabinwen@gmail.com>
2017-06-27 21:25:36 +0800

    添加header

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     <div id="app">
         <header class="header">
             <img class="logo" src="./assets/imgs/logo.jpg">
+            <div class="brand">Falcon Merlin</div>
         </header>
         <router-view></router-view>
     </div>
