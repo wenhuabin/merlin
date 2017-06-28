f2a1e4af2380f3c9618f8088dfc0ca3ad5513fe2
wenhuabin <huabinwen@gmail.com>
2017-06-28 09:23:18 +0800

    调整 About 页面样式

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,26 +3,27 @@
 </style>
 
 <template>
-  <div class="about">
-    <h1>{{ msg }}</h1>
-    <h2>Links</h2>
-    <ul>
-      <li><a href="https://huabinwen.com" target="_blank">Home Page</a></li>
-      <li><a href="https://github.com/wenhuabin" target="_blank">Github</a></li>
-      <li><a href="https://www.instagram.com/?hl=en" target="_blank">Instagram</a></li>
-      <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Hobby</h2>
-    <ul>
-      <li>看书</li>
-      <li>旅行</li>
-      <li>煮饭做菜</li>
-      <li>足球</li>
-      <li>羽毛球</li>
-      <li>写代码</li>
-    </ul>
-    <div>{{ $route.params.id}}</div> 
-  </div>
+	<div class="about">
+		<img class="launch" src="../assets/imgs/launch.jpg">
+		<h1>{{ msg }}</h1>
+		<h2>Links</h2>
+		<ul>
+		  <li><a href="https://huabinwen.com" target="_blank">Home Page</a></li>
+		  <li><a href="https://github.com/wenhuabin" target="_blank">Github</a></li>
+		  <li><a href="https://www.instagram.com/?hl=en" target="_blank">Instagram</a></li>
+		  <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
+		</ul>
+		<h2>Hobby</h2>
+		<ul>
+		  <li>看书</li>
+		  <li>旅行</li>
+		  <li>煮饭做菜</li>
+		  <li>足球</li>
+		  <li>羽毛球</li>
+		  <li>写代码</li>
+		</ul>
+		<div>{{ $route.params.id}}</div> 
+	</div>
 </template>
 
 <script>
