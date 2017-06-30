498792714aab61749553c95c8494cf72d437052d
wenhuabin <huabinwen@gmail.com>
2017-06-30 16:28:17 +0800

    通过props传递导航栏信息

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
             <img class="logo" src="./assets/imgs/logo.jpg">
             <div class="brand">Falcon Merlin</div>
         </header>
-        <nav-bar v-if="showNav"></nav-bar>
+        <nav-bar v-if="showNav" :navs='navs'></nav-bar>
         <router-view></router-view>
     </div>
 </template>
@@ -19,6 +19,13 @@ export default {
 	name: 'app',
 	data(){
 		return {
+			navs: [
+    		    {name: '主页', key: 0, url: '/about'},
+    		    {name: '人员管理', key: 1, url: '/', child: [{name: '会员列表', key: 10, url: '/', child: [{name: '会员列表', key: 30, url: '/'}, {name: '教练列表', key: 31, url: '/'}]}, {name: '教练列表', key: 11, url: '/'}]},
+    		    {name: '课程中心', key: 2, url: '/about'},
+    		    {name: '财务中心', key: 3, url: '/vue'},
+    		    {name: '系统设置', key: 4, url: '/vue'}
+    		  ]
 		}
 	},
 	computed:{
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -29,17 +29,18 @@
 
 export default {
   name: 'nav-bar',
+  props: ["navs"],
   data () {
     return {
       isCurrent: -1,
       isChildShow: [],
-      navs: [
-        {name: '主页', key: 0, url: '/about'},
-        {name: '人员管理', key: 1, url: '/', child: [{name: '会员列表', key: 10, url: '/', child: [{name: '会员列表', key: 30, url: '/'}, {name: '教练列表', key: 31, url: '/'}]}, {name: '教练列表', key: 11, url: '/'}]},
-        {name: '课程中心', key: 2, url: '/about'},
-        {name: '财务中心', key: 3, url: '/vue'},
-        {name: '系统设置', key: 4, url: '/vue'}
-      ],
+      //navs: [
+      //  {name: '主页', key: 0, url: '/about'},
+      //  {name: '人员管理', key: 1, url: '/', child: [{name: '会员列表', key: 10, url: '/', child: [{name: '会员列表', key: 30, url: '/'}, {name: '教练列表', key: 31, url: '/'}]}, {name: '教练列表', key: 11, url: '/'}]},
+      //  {name: '课程中心', key: 2, url: '/about'},
+      //  {name: '财务中心', key: 3, url: '/vue'},
+      //  {name: '系统设置', key: 4, url: '/vue'}
+      //],
     }
   },
   methods: {
