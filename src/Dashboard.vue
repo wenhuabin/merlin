f0a6af9be0557a52e6f2820e5c9ac854adb395b3
wenhuabin <huabinwen@gmail.com>
2017-07-02 00:02:03 +0800

    添加欢迎页

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -11,8 +11,10 @@
         <section id="main">
             <nav-bar :navs='navs'></nav-bar>
             <div class="welcome" v-if="isRoot">
-                helloword!
-
+                <img class="launch" src="./assets/imgs/launch.jpg">
+		        <h1>Welcome To Merlin</h1>
+		        <p>This Merlin Project is developed by WENHUABIN with Vue Framwork。</p>
+		        <footer class="footer">&copy Copyright 2017 by WENHUABIN. All Right Reserved</footer>
             </div>
             <div id="right">
                 <router-view></router-view>
@@ -28,7 +30,7 @@ export default {
 	data(){
 		return {
 	        navs: [
-                {name: '主页', key: 0, url: '/home'},
+                {name: '主页', key: 0, url: '/'},
                 {name: 'Demo', key: 1, url: '', child: [{name: '图形', key: 10, url: '/', child: [{name: 'Canvas', key: 110, url: '/'}, {name: 'SVG', key: 111, url: '/'}]}, {name: '图表', key: 11, url: '/'}]},
                 {name: '个人简介', key: 4, url: '', child: [{name: '技术栈', key: 40, url: '/skills'},{name: '工作经历', key: 41, url: '', child: [{name: '小熊快跑', key: 410, url: '/bear'}, {name: '蔚来汽车', key: 411, url: '/nio'}]},{name: '关于我', key: 42, url: '/about'}]},
             ]
