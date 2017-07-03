0359ccd3e39b51473a60f47f244b59658e302805
wenhuabin <huabinwen@gmail.com>
2017-07-03 21:34:16 +0800

    登录成功跳转到/目录

--- src/Login.vue
+++ src/Login.vue
@@ -46,7 +46,7 @@ export default {
 					this.$store.dispatch(types.SET_LOGIN_TOKEN, data.data)
                     sessionStorage.setItem('token', JSON.stringify(data.data)) 
 					this.err = '登录成功'
-					this.$router.push(this.$route.query.redirect ? this.$route.query.redirect : '/about');
+					this.$router.push(this.$route.query.redirect ? this.$route.query.redirect : '/');
 				}else{
 					this.err = '登录失败'
 				}
