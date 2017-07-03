5217e8ea24fe638689f8d8edd965e4aa0aaa4580
wenhuabin <huabinwen@gmail.com>
2017-07-03 10:05:31 +0800

    添加 设置和退出登录按钮；修改导航栏字体颜色

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -6,7 +6,9 @@
     <div id="content">
         <header class="header">
             <img class="logo" src="./assets/imgs/logo.jpg">
-            <div class="brand">行旅書讀</div>
+            <div class="brand">SpaceX</div>
+            <div class="logout">退出登录</div>
+            <div class="setting">设置</div>
         </header>
         <section id="main">
             <nav-bar :navs='navs'></nav-bar>
@@ -32,7 +34,7 @@ export default {
 	        navs: [
                 {name: '主页', key: 0, url: '/'},
                 {name: 'Demo', key: 1, url: '', child: [{name: '图形', key: 10, url: '/', child: [{name: 'Canvas', key: 110, url: '/'}, {name: 'SVG', key: 111, url: '/'}]}, {name: '图表', key: 11, url: '/'}]},
-                {name: '个人简介', key: 4, url: '', child: [{name: '技术栈', key: 40, url: '/skills'},{name: '工作经历', key: 41, url: '', child: [{name: '小熊快跑', key: 410, url: '/bear'}, {name: '蔚来汽车', key: 411, url: '/nio'}]},{name: '关于我', key: 42, url: '/about'}]},
+                {name: 'Profile', key: 4, url: '', child: [{name: 'Skills', key: 40, url: '/skills'},{name: 'Experience', key: 41, url: '', child: [{name: 'Bear', key: 410, url: '/bear'}, {name: 'Nio', key: 411, url: '/nio'}]},{name: 'About', key: 42, url: '/about'}]},
             ]
 		}
 	},
