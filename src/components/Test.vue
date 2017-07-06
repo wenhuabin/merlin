e29afab16b080ebf0c6c839f1cc1e487fc7b9bb9
wenhuabin <huabinwen@gmail.com>
2017-07-06 16:52:51 +0800

    添加Datepicker组件

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,8 +2,7 @@
 @import '~scss/web/test';
 </style>
 
-<template>
-	<div class="test">
+<template> <div class="test">
         <div class="welcome">
             <img class="launch" src="../assets/imgs/launch.jpg">
 		    <h1>Welcome To Merlin</h1>
@@ -13,6 +12,9 @@
                 <Selecting :initial_value='value' :list='list' @onChange='onChange'/>
                 <div class="hint">value: {{value}} / key: {{key}}</div>
             </div>
+			<div class="date-picker">
+				<DatePicker :date="startTime" :option="option" :limit="limit" />
+			</div>
             <Paging :page="page" :pages="pages" @onPageChange="onPageChange"/>
         </div>
 	</div>
@@ -21,9 +23,10 @@
 <script>
 import Paging from 'components/component/Paging'
 import Select from 'components/component/Select'
+import DatePicker from 'components/component/DatePicker'
 
 export default {
-  name: 'about',
+  name: 'Test',
   data () {
     return {
         page: 26,
@@ -31,6 +34,59 @@ export default {
         value: '请选择',
         key: -1,
         list: [{value: '测试1', disabled: false},{value: '测试2', disabled: false},{value: '测试3', disabled: true},{value: '测试4', disabled: false},{value: '非常长的非常长的条目条目5', disabled: false}],
+		startTime: {
+      	  time: ''
+      	},
+      	endtime: {
+      	  time: ''
+      	},
+		option: {
+            type: 'day',
+            week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+            month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            format: 'YYYY-MM-DD',
+            placeholder: 'when?',
+            inputStyle: {
+              'display': 'inline-block',
+              'padding': '6px',
+              'line-height': '22px',
+              'font-size': '16px',
+              'border': '2px solid #fff',
+              'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
+              'border-radius': '2px',
+              'color': '#5F5F5F'
+            },
+            color: {
+              header: '#ccc',
+              headerText: '#f00'
+            },
+            buttons: {
+              ok: 'Ok',
+              cancel: 'Cancel'
+            },
+            overlayOpacity: 0.5, // 0.5 as default
+            dismissible: true // as true as default
+        },
+        timeoption: {
+          type: 'min',
+          week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+          month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          format: 'YYYY-MM-DD HH:mm'
+        },
+        multiOption: {
+          type: 'multi-day',
+          week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+          month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          format:"YYYY-MM-DD HH:mm"
+        },
+        limit: [{
+          type: 'weekday',
+          available: [1, 2, 3, 4, 5]
+        },{
+          type: 'fromto',
+          from: '2016-02-01',
+          to: '2016-02-20'
+        }]
     }
   },
   methods: {
@@ -45,6 +101,7 @@ export default {
   components: {
     'Paging': Paging,
     'Selecting': Select,
-  }
+    'DatePicker': DatePicker,
+  },
 }
 </script>
