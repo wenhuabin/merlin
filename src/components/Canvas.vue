c384b8543370e736030bfbdb260cee6214bba7bc
wenhuabin <huabinwen@gmail.com>
2017-07-13 11:50:32 +0800

    添加canvas图形

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,32 +1,95 @@
 <style lang="scss" scoped>
-@import '~scss/web/canvas';
+@import '~scss/web/canvas'
 </style>
 
 <template>
     <div class="canvas-demo">
         <h1>Canvas</h1>
+		<h2>1、直线</h2>
+        <div class="demo">
+            <canvas id="canvas-line" width="400" height="250">您的浏览器不支持 HTML5 canvas 标签。</canvas>
+        </div>
+		<h2>2、圆形</h2>
+        <div class="demo">
+            <canvas id="canvas-circle" width="400" height="250">您的浏览器不支持 HTML5 canvas 标签。</canvas>
+        </div>
+		<h2>3、文字</h2>
+        <div class="demo">
+            <canvas id="canvas-text" width="400" height="250">您的浏览器不支持 HTML5 canvas 标签。</canvas>
+        </div>
+		<h2>4、渐变</h2>
+        <div class="demo">
+            <canvas id="canvas-gran" width="400" height="250">您的浏览器不支持 HTML5 canvas 标签。</canvas>
+        </div>
     </div>
 </template>
 
 <script>
 
 export default {
-  name: 'canvas',
-  data () {
-    return {
-        page: 5,
-        pages: 26,
-        value: '请选择',
-        key: -1,
-        list: [{value: '测试1', disabled: false},{value: '测试2', disabled: false},{value: '测试3', disabled: false},{value: '测试4', disabled: false},{value: '非常长的非常长的条目条目5', disabled: false}],
-        date: moment().format("YYYY-MM-DD"),
-    }
-  },
-  methods: {
-      onPageChange: function(p){
-      },
-  },
-  components: {
-  },
+    name: 'canvas',
+    data () {
+      return {
+      }
+    },
+    mounted(){
+        this.drawLine()
+        this.drawCircle()
+        this.drawText()
+        this.drawGran()
+    },
+    methods: {
+        drawLine: function(){
+            var c=document.getElementById("canvas-line")
+            var ctx=c.getContext("2d")
+            ctx.moveTo(0,0)
+            ctx.lineTo(200,100)
+            ctx.stroke()
+        },
+        drawCircle: function(){
+            var c=document.getElementById("canvas-circle")
+            var ctx=c.getContext("2d")
+            //ctx.beginPath()
+            //ctx.arc(200,125,80,0,2*Math.PI)
+            //ctx.stroke()
+            //ctx.beginPath()
+            //ctx.arc(100,125,80,0,2*Math.PI)
+            //ctx.stroke()
+            //ctx.beginPath()
+            //ctx.arc(300,125,80,0,2*Math.PI)
+            //ctx.stroke()
+            ctx.beginPath();
+            ctx.arc(75,75,50,0,Math.PI*2,true);  // Outer circle
+            
+            ctx.moveTo(110,75);
+            ctx.arc(75,75,35,0,Math.PI,false);   // Mouth
+            
+            ctx.moveTo(65,65);
+            ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+            
+            ctx.moveTo(95,65);
+            ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+            
+            ctx.stroke();
+        },
+        drawText: function(){
+            var c=document.getElementById("canvas-text")
+            var ctx=c.getContext("2d")
+            ctx.font="30px Arial"
+            ctx.fillText("Hello World",100,100)
+        },
+        drawGran: function(){
+            var c=document.getElementById("canvas-gran")
+            var ctx=c.getContext("2d")
+            var grd=ctx.createLinearGradient(0,0,400,0)
+            //var grd=ctx.createRadialGradient(75,50,5,90,60,100)
+            grd.addColorStop(0,"red")
+            grd.addColorStop(1,"white")
+            ctx.fillStyle=grd
+            ctx.fillRect(0,0,400,250)
+        },
+    },
+    components: {
+    },
 }
 </script>
