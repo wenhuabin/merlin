44c338913c98cf65ece6ced17b1f4cce236f22db
wenhuabin <huabinwen@gmail.com>
2017-07-13 09:52:40 +0800

    去除webpack调试信息

--- build/build.js
+++ build/build.js
@@ -9,7 +9,6 @@ var chalk = require('chalk')
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
-console.log(webpackConfig);
 
 var spinner = ora('building for production...')
 spinner.start()
