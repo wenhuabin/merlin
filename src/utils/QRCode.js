e91d32fb5dc7830d04d9319b7b391ab0840c781b
wenhuabin <huabinwen@gmail.com>
2017-07-17 10:08:29 +0800

    调整二维码中间图片大小

--- src/utils/QRCode.js
+++ src/utils/QRCode.js
@@ -4,20 +4,14 @@ export default ({
     canvas,
     context,
     width=180,
-    height=180
+    height=180,
+	src = "/static/img/logo.7c0592a.jpg"
 }) => {
     const qrcode = qr(context);
     canvas.width = width;
     canvas.height = height;
 
     var ctx = canvas.getContext('2d');
-
-	//ctx.fillStyle='#fff';
-	////ctx.arc(90,90,35,0,2*Math.PI);
-	////ctx.fill();
-	////ctx.beginPath();
-	//ctx.arc(90,90,30,0,2*Math.PI);
-	//ctx.fill();
 
     var cells = qrcode.modules;
 
@@ -33,10 +27,10 @@ export default ({
             ctx.fillRect(Math.round(c*tileW), Math.round(r*tileH), w, h);
         }
     }
-	ctx.clearRect(55,55,70,70);
+	ctx.clearRect(60,60,65,65);
 	var image = new Image()
 	image.onload = function() {
-    	ctx.drawImage(image, 60, 60, 60, 60)
+    	ctx.drawImage(image, 65, 65, 55, 55)
 	};
-	image.src = "/static/img/logo.7c0592a.jpg"
+	image.src = src 
 };
