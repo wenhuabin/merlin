d364cc58ae79e803e83ba2451bfa78dc3066f6c3
wenhuabin <huabinwen@gmail.com>
2017-07-25 11:07:28 +0800

    测试中文显示

--- src/components/component/DatePicker.vue
+++ src/components/component/DatePicker.vue
@@ -73,6 +73,8 @@ export default {
         return {
           type: 'day',
           SundayFirst: true,
+          //week: ['日', '一', '二', '三', '四', '五', '六'],
+          //month: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
           week: ['SUN', 'MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT'],
           month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
           format: 'YYYY-MM-DD',
