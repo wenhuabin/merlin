cba6893b087abd9137b279968b564cd6d0e5906b
wenhuabin <huabinwen@gmail.com>
2017-08-24 12:01:08 +0800

    添加地图显示

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,6 +5,7 @@
 <template>
     <div class="map-demo">
         <h1>高德地图集成测试</h1>
+		<div id="map-container" tabindex="0"></div>
     </div>
 </template>
 
@@ -15,10 +16,15 @@ export default {
     name: 'svg',
     data () {
         return {
+			map: null,
         }
     },
     mounted(){
-        
+		var map = new AMap.Map('map-container',{
+            resizeEnable: true,
+            zoom: 10,
+            center: [116.480983, 40.0958]
+        });
     },
     components: {
     },
