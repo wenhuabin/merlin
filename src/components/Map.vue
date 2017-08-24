10b959daecfc4c309e4cff16552845e79835c85b
wenhuabin <huabinwen@gmail.com>
2017-08-24 18:28:32 +0800

    update map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,336 +18,79 @@ export default {
         }
     },
     mounted(){
-		var map = new AMap.Map('map-container',{
-            resizeEnable: true,
-            zoom: 10
-        });
-
-		AMapUI.load(['ui/misc/PointSimplifier', 'lib/$', 'lib/utils'], function(PointSimplifier, $, utils) {
-
-        	if (!PointSimplifier.supportCanvas) {
-        	    alert('当前环境不支持 Canvas！');
-        	    return;
-        	}
-
-        	var defaultRenderOptions = {
-        	    drawQuadTree: false,
-        	    drawPositionPoint: false,
-        	    drawShadowPoint: false,
-        	    //点
-        	    pointStyle: {
-        	        content: 'circle',
-        	        width: 6,
-        	        height: 6,
-        	        fillStyle: '#1f77b4',
-        	        lineWidth: 1,
-        	        strokeStyle: 'rgba(0,0,0,0)'
-        	    },
-        	    //TopN区域
-        	    topNAreaStyle: {
-        	        autoGlobalAlphaAlpha: true,
-        	        content: 'rect',
-        	        fillStyle: '#e25c5d',
-        	        lineWidth: 1,
-        	        strokeStyle: 'rgba(0,0,0,0)'
-        	    },
-        	    //点的硬核部分
-        	    pointHardcoreStyle: {
-        	        content: 'none',
-        	        width: 5,
-        	        height: 5,
-        	        lineWidth: 1,
-        	        fillStyle: 'rgba(0,0,0,0)',
-        	        strokeStyle: 'rgba(0,0,0,0)'
-        	    },
-        	    //定位点
-        	    pointPositionStyle: {
-        	        content: 'circle',
-        	        width: 2,
-        	        height: 2,
-        	        lineWidth: 1,
-        	        //offset: ['-50%', '-50%'],
-        	        strokeStyle: 'rgba(0,0,0,0)',
-        	        fillStyle: '#cc0000'
-        	    },
-        	    //鼠标hover时的覆盖点
-        	    pointHoverStyle: {
-        	        width: 10,
-        	        height: 10,
-        	        content: 'circle',
-        	        fillStyle: 'rgba(0,0,0,0)',
-        	        lineWidth: 2,
-        	        strokeStyle: '#ffa500'
-        	    },
-        	    //空间被占用的点
-        	    shadowPointStyle: {
-        	        fillStyle: 'rgba(0,0,0,0.2)',
-        	        content: 'circle',
-        	        width: 6,
-        	        height: 6,
-        	        lineWidth: 1,
-        	        strokeStyle: 'rgba(0,0,0,0)'
-        	    }
-        	};
-        	var pointSimplifierIns = new PointSimplifier({
-
-        	    zIndex: 100,
-
-        	    map: map,
-
-        	    getPosition: function(item) {
-
-        	        if (!item) {
-        	            return null;
-        	        }
-
-        	        var parts = item.split(',');
-
-        	        return [parseFloat(parts[0]), parseFloat(parts[1])];
-        	    },
-        	    compareDataItem: function(a, b, aIndex, bIndex) {
-
-        	        //数据源尾部的优先
-        	        return aIndex > bIndex ? -1 : 1;
-        	    },
-        	    getHoverTitle: function(dataItem, idx) {
-        	        return idx + ': ' + dataItem;
-        	    },
-        	    renderOptions: defaultRenderOptions
-        	});
-
-        	window.pointSimplifierIns = pointSimplifierIns;
-
-        	$.get('http://a.amap.com/amap-ui/static/data/10w.txt', function(csv) {
-
-        	    var data = csv.split('\n');
-
-        	    pointSimplifierIns.setData(data);
-
-        	    $('#loadingTip').remove();
-        	});
-
-        	var customContainer = document.getElementById('my-gui-container');
-
-        	function createRenderEngGui() {
-
-        	    function RenderEngOptions() {
-        	        this.drawQuadTree = false;
-        	        this.drawPositionPoint = false;
-        	        this.drawShadowPoint = false;
-        	        this.disableHardcoreWhenPointsNumBelow = 0;
-        	    }
-
-        	    var renderEngParams = new RenderEngOptions();
-
-        	    var renderEngGui = new dat.GUI({
-        	        width: 260,
-        	        autoPlace: false,
-        	    });
-
-        	    renderEngGui.add(renderEngParams, 'drawQuadTree').onChange(render);
-
-        	    renderEngGui.add(renderEngParams, 'drawShadowPoint').onChange(render);
-
-        	    renderEngGui.add(renderEngParams, 'drawPositionPoint').onChange(render);
-
-        	    //renderEngGui.add(renderEngParams, 'disableHardcoreWhenPointsNumBelow', 0, 10000).step(1000).onChange(render);
-
-        	    addGuiPanel('', '', renderEngGui);
-
-        	    return renderEngParams;
-        	}
-
-        	var customContentMap = {
-        	    'custom_path': function setCustom1Content(params) {
-
-        	        return utils.extend(params, {
-        	            content: function(ctx, x, y, width, height) {
-
-        	                //注意，这里的width和height可能不同于pointStyle里面的width/height， 高清屏幕下会存在比例缩放
-
-        	                //这里绘制一个菱形路径
-        	                var yPos = 1 / 3;
-
-        	                var top = [x + width / 2, y],
-        	                    right = [x + width, y + height * yPos],
-        	                    bottom = [x + width / 2, y + height],
-        	                    left = [x, y + height * yPos];
-
-        	                ctx.moveTo(top[0], top[1]);
-        	                ctx.lineTo(right[0], right[1]);
-        	                ctx.lineTo(bottom[0], bottom[1]);
-        	                ctx.lineTo(left[0], left[1]);
-        	                ctx.lineTo(top[0], top[1]);
-
-        	            },
-        	            //定位点为底部中心
-        	            offset: ['-50%', '-100%']
-        	        });
-        	    },
-        	    'custom_icon': function setCustom3Content(params) {
-
-        	        return utils.extend(params, {
-        	            //使用图片
-        	            content: PointSimplifier.Render.Canvas.getImageContent(
-        	                'http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
-        	                function onload() {
-        	                    pointSimplifierIns.renderLater();
-        	                },
-        	                function onerror(e) {
-        	                    alert('图片加载失败！');
-        	                }),
-        	            //定位点为底部中心
-        	            offset: ['-50%', '-100%']
-        	        });
-        	    }
-        	};
-
-        	function createPointStyleGui(target) {
-
-        	    var pointStyleGui = new dat.GUI({
-        	        width: 260,
-        	        autoPlace: false,
-        	    });
-
-        	    var pointStyleParams = utils.extend({}, defaultRenderOptions[target]);
-
-        	    //pointStyleGui.add(pointStyleParams, 'optionName');
-
-        	    //形状类型
-        	    pointStyleGui.add(pointStyleParams, 'content', ['rect', 'circle', 'none', 'custom_path', 'custom_icon']).onChange(render);
-
-        	    if (target !== 'topNAreaStyle') {
-
-        	        //宽度
-        	        pointStyleGui.add(pointStyleParams, 'width', 0, 50).step(1).onChange(render);
-
-        	        //高度
-        	        pointStyleGui.add(pointStyleParams, 'height', 0, 50).step(1).onChange(render);
-
-        	    } else {
-        	        pointStyleGui.add(pointStyleParams, 'autoGlobalAlphaAlpha').onChange(render);
-        	    }
-
-        	    pointStyleGui.addColor(pointStyleParams, 'fillStyle').onChange(render);
-
-        	    pointStyleGui.addColor(pointStyleParams, 'strokeStyle').onChange(render);
-
-        	    pointStyleGui.add(pointStyleParams, 'lineWidth', 1, 10).step(1).onChange(render);
-
-        	    addGuiPanel(target, target, pointStyleGui);
-
-        	    return pointStyleParams;
-        	}
-
-        	function addGuiPanel(id, title, gui) {
-
-        	    var container = document.createElement('div');
-
-        	    container.id = id;
-
-        	    if (title) {
-        	        var tEle = document.createElement('h3');
-        	        tEle.innerHTML = title;
-        	        container.appendChild(tEle);
-        	    }
-
-        	    container.appendChild(gui.domElement);
-
-        	    customContainer.appendChild(container);
-        	}
-
-        	var styleOptions = ['pointStyle', 'topNAreaStyle', 'pointHardcoreStyle', 'pointPositionStyle', 'pointHoverStyle', 'shadowPointStyle'];
-
-        	var renderEngParams = createRenderEngGui(),
-        	    styleParamsMap = {};
-
-        	for (var i = 0, len = styleOptions.length; i < len; i++) {
-        	    styleParamsMap[styleOptions[i]] = createPointStyleGui(styleOptions[i]);
-        	}
-
-        	function render() {
-
-        	    $('#shadowPointStyle').toggleClass('hide', !renderEngParams.drawShadowPoint);
-
-        	    $('#pointPositionStyle').toggleClass('hide', !renderEngParams.drawPositionPoint);
-
-        	    pointSimplifierIns.renderEngine.setOptions(renderEngParams);
-
-        	    for (var k in styleParamsMap) {
-
-        	        var params = utils.extend({}, styleParamsMap[k]);
-
-        	        if (customContentMap[params['content']]) {
-        	            params = customContentMap[params['content']](params);
-        	        }
-
-        	        pointSimplifierIns.renderEngine.setOption(k, params);
-        	    }
-
-        	    pointSimplifierIns.renderLater(200);
-
-        	    refreshConfigPanel();
-        	}
-
-        	var colorFlds = ['fillStyle', 'strokeStyle', 'borderStyle'],
-        	    rgbAlphaRegx = /([\d\.]+)\s*\)/i;
-
-        	function isEmptyColor(color) {
-
-        	    if (color.indexOf('rgba') !== 0) {
-        	        return false;
-        	    }
-
-        	    var match = color.match(rgbAlphaRegx);
-
-        	    if (match && parseFloat(match[1]) < 0.01) {
-        	        return true;
-        	    }
-
-        	    return false;
-        	}
-
-        	function fixColors(opts) {
-
-        	    if (utils.isObject(opts)) {
-
-        	        for (var i = 0, len = colorFlds.length; i < len; i++) {
-
-        	            if (opts[colorFlds[i]] && isEmptyColor(opts[colorFlds[i]])) {
-        	                opts[colorFlds[i]] = null;
-        	            }
-        	        }
-        	    }
-
-        	    return opts;
-        	}
-
-
-        	function exportRenderOptions() {
-
-        	    var options = utils.extend({}, renderEngParams);
-
-        	    for (var k in defaultRenderOptions) {
-
-        	        var opts = styleParamsMap[k];
-
-        	        if (opts) {
-        	            options[k] = fixColors(opts);
-        	        }
-        	    }
-        	    return options;
-        	}
-
-        	function refreshConfigPanel() {
-
-        	    var options = exportRenderOptions();
-
-        	    var configStr = 'renderOptions: ' + JSON.stringify(options, null, 2);
-
-        	}
-        	render();
+		var map = new AMap.Map('map-container', {
+    	    zoom: 4
     	});
+		AMap.plugin(['AMap.ToolBar','AMap.Scale'],
+		    function(){
+		        map.addControl(new AMap.ToolBar());
+		        map.addControl(new AMap.Scale());
+		});
+		//加载PointSimplifier，loadUI的路径参数为模块名中 'ui/' 之后的部分 
+		AMapUI.loadUI(['misc/PointSimplifier'], function(PointSimplifier) {
+		
+		    if (!PointSimplifier.supportCanvas) {
+		        alert('当前环境不支持 Canvas！');
+		        return;
+		    }
+		
+		    //启动页面
+		    initPage(PointSimplifier);
+		});
+		
+		function initPage(PointSimplifier) {
+		    //创建组件实例
+		    var pointSimplifierIns = new PointSimplifier({
+		        map: map, //关联的map
+		        compareDataItem: function(a, b, aIndex, bIndex) {
+		            //数据源中靠后的元素优先，index大的排到前面去
+		            return aIndex > bIndex ? -1 : 1;
+		        },
+		        getPosition: function(dataItem) {
+		            //返回数据项的经纬度，AMap.LngLat实例或者经纬度数组
+		            return dataItem.position;
+		        },
+		        getHoverTitle: function(dataItem, idx) {
+		            //返回数据项的Title信息，鼠标hover时显示
+		            return '序号: ' + idx;
+		        },
+		        renderOptions: {
+		            //点的样式
+		            pointStyle: {
+		                fillStyle: 'blue' //蓝色填充
+		            }
+		        }
+		    });
+		
+		    //随机创建一批点，仅作示意
+		    var data = createPoints(map.getCenter(), 100000);
+			console.log(map)
+		
+		    //设置数据源，data需要是一个数组
+		    pointSimplifierIns.setData(data);
+		
+		    //监听事件
+		    pointSimplifierIns.on('pointClick pointMouseover pointMouseout', function(e, record) {
+		        console.log(e.type, record);
+		    });
+		}
+		
+		//仅作示意
+		function createPoints(center, num) {
+		    var data = [];
+		    for (var i = 0, len = num; i < len; i++) {
+		        data.push({
+		            position: [
+		                center.getLng() + (Math.random() > 0.5 ? 1 : -1) * Math.random(),
+		                center.getLat() + (Math.random() > 0.5 ? 1 : -1) * Math.random()
+		            ]
+		        });
+		    }
+		    return data;
+		}
+
+    	
+		
     },
     components: {
     },
