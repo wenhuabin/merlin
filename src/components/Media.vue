59c38a9689970bae9a3c9bf85769e25efaec683b
wenhuabin <huabinwen@gmail.com>
2017-08-25 12:46:39 +0800

    add ifram music

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -6,6 +6,11 @@
     <div class="media-demo">
         <h1>HTML 5 多媒体</h1>
 		<div id="map-container" tabindex="0"></div>
+        <iframe class="ifram-netmusic" frameborder="no" border="0" marginwidth="0" marginheight="0" width=100% height=120 src="http://music.163.com/outchain/player?type=2&id=299757&auto=1&height=66"></iframe>
+        <audio controls="controls">
+              <source src="https://music.163.com/#/song?id=18098592" type="audio/mp3">
+              Your browser does not support the audio element.
+        </audio>
     </div>
 </template>
 
