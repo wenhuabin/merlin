bed1bbf7b54ef6e51ab96c2b898f02cdfd3b136f
wenhuabin <huabinwen@gmail.com>
2017-08-25 18:21:12 +0800

    move map to data

--- src/components/BaiduMap.vue
+++ src/components/BaiduMap.vue
@@ -18,16 +18,17 @@ export default {
         }
     },
     mounted(){
-        var map = new BMap.Map("map-container");          // 创建地图实例  
+        this.map = new BMap.Map("map-container");          // 创建地图实例  
         var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
-        map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
-        map.addControl(new BMap.NavigationControl({
+        this.map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
+        this.map.addControl(new BMap.NavigationControl({
+            offset: new BMap.Size(0, 10),
             anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
         }));    
-        map.addControl(new BMap.ScaleControl());    
-        map.addControl(new BMap.OverviewMapControl());    
-        map.addControl(new BMap.MapTypeControl());    
-        map.setCurrentCity("北京"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
+        this.map.addControl(new BMap.ScaleControl());    
+        this.map.addControl(new BMap.OverviewMapControl());    
+        this.map.addControl(new BMap.MapTypeControl());    
+        this.map.setCurrentCity("北京"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
     },
     components: {
     },
