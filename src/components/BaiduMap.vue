8970bcd8f501a3e7b2d1ccff4e44b19c03a61d9c
wenhuabin <huabinwen@gmail.com>
2017-08-25 18:24:22 +0800

    use vue properties in map

--- src/components/BaiduMap.vue
+++ src/components/BaiduMap.vue
@@ -18,17 +18,22 @@ export default {
         }
     },
     mounted(){
-        this.map = new BMap.Map("map-container");          // 创建地图实例  
-        var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
-        this.map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
-        this.map.addControl(new BMap.NavigationControl({
-            offset: new BMap.Size(0, 10),
-            anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
-        }));    
-        this.map.addControl(new BMap.ScaleControl());    
-        this.map.addControl(new BMap.OverviewMapControl());    
-        this.map.addControl(new BMap.MapTypeControl());    
-        this.map.setCurrentCity("北京"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
+        this.init()
+    },
+    methods: {
+        init: function(){
+            this.map = new BMap.Map("map-container")          // 创建地图实例  
+            var point = new BMap.Point(116.404, 39.915)  // 创建点坐标  
+            this.map.centerAndZoom(point, 15)                 // 初始化地图，设置中心点坐标和地图级别
+            this.map.addControl(new BMap.NavigationControl({
+                offset: new BMap.Size(0, 10),
+                anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
+            }))    
+            this.map.addControl(new BMap.ScaleControl())    
+            this.map.addControl(new BMap.OverviewMapControl())    
+            this.map.addControl(new BMap.MapTypeControl())    
+            this.map.setCurrentCity("北京") // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
+        },
     },
     components: {
     },
