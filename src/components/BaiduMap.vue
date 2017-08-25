f50202cd76ebf570eefdcd1e70458acfff79236a
wenhuabin <huabinwen@gmail.com>
2017-08-25 18:19:05 +0800

    add controls

--- src/components/BaiduMap.vue
+++ src/components/BaiduMap.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="bmap-demo">
-        <h1>百度地图集成测试</h1>
+        <h1>百度地图</h1>
 		<div id="map-container" tabindex="0"></div>
     </div>
 </template>
@@ -21,7 +21,13 @@ export default {
         var map = new BMap.Map("map-container");          // 创建地图实例  
         var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
         map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
-		
+        map.addControl(new BMap.NavigationControl({
+            anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
+        }));    
+        map.addControl(new BMap.ScaleControl());    
+        map.addControl(new BMap.OverviewMapControl());    
+        map.addControl(new BMap.MapTypeControl());    
+        map.setCurrentCity("北京"); // 仅当设置城市信息时，MapTypeControl的切换功能才能可用
     },
     components: {
     },
