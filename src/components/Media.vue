ac206c3a2cf5f88a0d658f667db9d7dd5c5370f5
wenhuabin <huabinwen@gmail.com>
2017-08-30 23:30:54 +0800

    add control button

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -5,14 +5,18 @@
 <template>
     <div class="media-demo">
         <h1>HTML 5 多媒体</h1>
-		<div id="map-container" tabindex="0"></div>
-        <audio controls="controls">
+        <audio id="audio-element" controls="controls">
               <source src="https://music.163.com/#/song?id=18098592" type="audio/mp3">
               Your browser does not support the audio element.
         </audio>
-        <video src="http://v2v.cc/~j/theora_testsuite/320x240.ogg" controls>
+        <video id="video-element" src="http://v2v.cc/~j/theora_testsuite/320x240.ogg" controls>
           你的浏览器不支持 <code>video</code> 标签.
         </video>
+        <div class="controls">
+            <button class="btn" @click="stopVideo">暂停</button>
+            <button class="btn" @click="startVideo">开始</button>
+            <button class="btn" @click="startVideoAtPos">从2S 开始</button>
+        </div>
     </div>
 </template>
 
@@ -22,10 +26,25 @@ export default {
     name: 'media',
     data () {
         return {
+            audio: null,
+            video: null,
         }
     },
     mounted(){
-		
+        this.audio = document.getElementById('audio-element')
+        this.video = document.getElementById('video-element')
+    },
+    methods: {
+        startVideo: function(){
+            this.video.play()
+        },
+        stopVideo: function(){
+            this.video.pause()
+        },
+        startVideoAtPos: function(){
+            this.video.currentTime = 2
+        },
+
     },
     components: {
     },
