01cc184da987bcc91d8946bf976e2b3d1f7b7ca2
wenhuabin <huabinwen@gmail.com>
2017-09-07 17:46:34 +0800

    update chart data directly by change option

--- src/components/Echart.vue
+++ src/components/Echart.vue
@@ -70,6 +70,45 @@ export default {
 		this.fillPie()
     },
     mounted(){
+		setTimeout(()=>{
+			this.pie = {
+			    title : {
+			        text: '某站点用户访问来源',
+					align: 'center',
+			        x:'center'
+			    },
+			    tooltip : {
+			        trigger: 'item',
+			        formatter: "{a} <br/>{b} : {c} ({d}%)"
+			    },
+			    legend: {
+			        orient: 'horizontal',
+			        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+					top: 50,
+			    },
+			    series : [{
+			            name: '访问来源',
+			            type: 'pie',
+			            radius : '55%',
+			            center: ['50%', '60%'],
+			            data:[
+			                {value:335, name:'直接访问'},
+			                {value:310, name:'邮件营销'},
+			                {value:234, name:'联盟广告'},
+			                {value:135, name:'视频广告'},
+			                {value:548, name:'搜索引擎'}
+			            ],
+			            itemStyle: {
+			                emphasis: {
+			                    shadowBlur: 10,
+			                    shadowOffsetX: 0,
+			                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+			                }
+			            }
+			        }
+			    ]
+			}
+		}, 3000)
     },
     methods: {
 		fillPie: function(){
