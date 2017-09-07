6a99db92f7a6187329d33e9fe8d700b2a91c30f5
wenhuabin <huabinwen@gmail.com>
2017-09-07 17:02:18 +0800

    render pie

--- src/components/Echart.vue
+++ src/components/Echart.vue
@@ -20,6 +20,7 @@
         </div>
 		<h2>5、饼图</h2>
         <div class="demo">
+            <chart :options="pie"></chart>
         </div>
 		<h2>6、雷达图</h2>
         <div class="demo">
@@ -60,17 +61,56 @@ export default {
       return {
 	  	  polar: {}, 
 		  line: {},
+		  pie: {},
       }
     },
     created(){
 		this.fillPolar()
 		this.fillLine()
+		this.fillPie()
     },
     mounted(){
-		//this.fillPolar()
-		//this.fillLine()
     },
     methods: {
+		fillPie: function(){
+			this.pie = {
+			    title : {
+			        text: '某站点用户访问来源',
+			        subtext: '纯属虚构',
+			        x:'center'
+			    },
+			    tooltip : {
+			        trigger: 'item',
+			        formatter: "{a} <br/>{b} : {c} ({d}%)"
+			    },
+			    legend: {
+			        orient: 'vertical',
+			        left: 'left',
+			        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+			    },
+			    series : [{
+			            name: '访问来源',
+			            type: 'pie',
+			            radius : '55%',
+			            center: ['50%', '60%'],
+			            data:[
+			                {value:335, name:'直接访问'},
+			                {value:310, name:'邮件营销'},
+			                {value:234, name:'联盟广告'},
+			                {value:135, name:'视频广告'},
+			                {value:1548, name:'搜索引擎'}
+			            ],
+			            itemStyle: {
+			                emphasis: {
+			                    shadowBlur: 10,
+			                    shadowOffsetX: 0,
+			                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+			                }
+			            }
+			        }
+			    ]
+			}
+		},
 		fillLine: function(){
 			this.line = {
     			title: {
