df62edc88f377b855b3629bb72f8de2ce309f218
wenhuabin <huabinwen@gmail.com>
2017-11-24 22:17:01 +0800

    add menu item

--- src/components/component/MenuItem.vue
+++ src/components/component/MenuItem.vue
@@ -3,8 +3,13 @@
 </style>
 
 <template>
-    <div>
-    </div>
+    <li class="item" ref="item" :style="styleArr" @click="changeShowItem">
+      <transition :name="currentIndex === index?'item-selected':'item-not-selected'">
+        <div class="item-wrapper" v-show="showItem"  @animationend="animationEnd">
+          <button class="item-btn" :class="[icon]"></button>
+        </div>
+      </transition>
+    </li>
 </template>
 
 <script>
