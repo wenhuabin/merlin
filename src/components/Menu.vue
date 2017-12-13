9a9deaa11f5013eb11e368d2ed4cf9dec30d6840
wenhuabin <huabinwen@gmail.com>
2017-12-13 11:08:55 +0800

    hide animation example

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,12 +9,14 @@
             <Menu-Comp />
         </div>
 		<!-- 首先将要过渡的元素用transition包裹，并设置过渡的name，然后添加触发这个元素过渡的按钮（实际项目中不一定是按钮，任何能触发过渡组件的DOM操作的操作都可以） -->
+        <!--
 		<div class="transition-box">
             <button class="btn" @click="show=!show">{{show ? '隐藏' : '显示'}}</button>
 		  	<transition name="fade">
 		  	  	<p v-show="show">hello</p>
 		  	</transition>
 		</div>
+        -->
     </div>
 </template>
 
