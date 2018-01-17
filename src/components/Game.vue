04203fc6bc4081fb41727c6a2d67e0ef73df77db
wenhuabin <huabinwen@gmail.com>
2018-01-17 09:58:02 +0800

    add block

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,7 +5,37 @@
 <template>
     <div class="game-demo">
         <h1>动画优化测试</h1>
-		
+        <div class="carousel-container">
+        </div>
+        <div class="grid-container">
+            <div class="box">
+                1
+            </div>
+            <div class="box">
+                2
+            </div>
+            <div class="box">
+                3
+            </div>
+            <div class="box">
+                4
+            </div>
+            <div class="box">
+                开始
+            </div>
+            <div class="box">
+                5
+            </div>
+            <div class="box">
+                6
+            </div>
+            <div class="box">
+                7
+            </div>
+            <div class="box">
+                8
+            </div>
+        </div>
     </div>
 </template>
 
