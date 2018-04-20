5167e0ce44917df1a028033ee406aacb7d1b0245
wenhuabin <huabinwen@gmail.com>
2018-04-20 11:33:43 +0800

    add play notification

--- src/components/Media.vue
+++ src/components/Media.vue
@@ -17,6 +17,12 @@
             <button class="btn" @click="startVideo">开始</button>
             <button class="btn" @click="startVideoAtPos">从2S 开始</button>
         </div>
+        <div :style="{height: '0'}">
+            <audio id="sound"><source src="https://freesound.org/data/previews/234/234524_4019029-lq.mp3" type="audio/mp3" /></audio>
+        </div>
+        <div class="controls">
+            <button class="btn" @click="showNotification">播放通知</button>
+        </div>
     </div>
 </template>
 
@@ -28,23 +34,44 @@ export default {
         return {
             audio: null,
             video: null,
+            notification: null,
         }
     },
     mounted(){
         this.audio = document.getElementById('audio-element')
         this.video = document.getElementById('video-element')
+        this.notification = document.getElementById('sound')
+        Notification.requestPermission().then(function(permission){
+            console.log(permission);
+        })
     },
     methods: {
-        startVideo: function(){
+        startVideo(){
             this.video.play()
         },
-        stopVideo: function(){
+        stopVideo(){
             this.video.pause()
         },
-        startVideoAtPos: function(){
+        startVideoAtPos(){
             this.video.currentTime = 2
         },
+        showNotification(){
+            this.playSound();
+            if (Notification.permission == "granted") {
+                this.playSound();
+                let notification = new Notification("通知", {
+                    body: '通知内容',
+                    icon: '/imgs/logo.7c0592ae.jpg',
+                })
 
+                notification.onclick = function() {
+                    notification.close();
+                }
+            }
+        },
+        playSound(){
+            this.notification.play()
+        }
     },
     components: {
     },
